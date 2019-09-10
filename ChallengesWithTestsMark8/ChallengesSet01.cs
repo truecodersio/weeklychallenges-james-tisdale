1acb812a9e75f954039b14111aca1b3803734cee
James Tisdale <james.tisdale89@gmail.com>
2019-09-10 15:23:29 -0500

    finished challenges

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,37 +6,62 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            bool same = (num1 == num2);
+            return same;  
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double answer = minuend - subtrahend;
+            return answer;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int theAnswer = number1 + number2;
+            return theAnswer;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if(number1 == number2)
+            {
+                return number2;
+            }
+            else
+            {
+                if(number1 > number2)
+                {
+                    return number2;
+                }
+                else
+                {
+                    return number1;
+                }
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long multAnswer = factor1 * factor2;
+            return multAnswer;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if(nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
